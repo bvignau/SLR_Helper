74d478352131e3a57abc5f5fc26122af38642ea4
benjamin <benjamin.vignau@insa-cvl.fr>
2019-10-16 01:51:45 -0400

    improved update.sh

--- update.sh
+++ update.sh
@@ -4,9 +4,10 @@ if [ -f "/usr/bin/slrh" ];then
 else :
 	echo "slrh installation"
 	echo "#!/bin/bash" > /usr/bin/slrh
-	echo "python3 /usr/bin/SLRHelper_0.1/main.py $1" >> /usr/bin/slrh
+	echo "python3 /usr/bin/SLRHelper_0.1/main.py \$1" >> /usr/bin/slrh
 	chmod +x /usr/bin/slrh
 fi
 if [ -d "/usr/bin/SLRHelper_0.1" ]; then
 	rm -rf /usr/bin/SLRHelper_0.1/
+fi
 cp -r SLRHelper_0.1/ /usr/bin
